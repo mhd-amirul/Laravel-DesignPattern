2058cccc63b719dca315c18e3a1f9656ad9f38a7
mhd-amirul <amirulm236@gmail.com>
2023-02-06 23:56:08 +0800

    feat: Unit test for design pattern

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,15 +7,31 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    public function test_the_application_returns_a_successful_response_if_code_200_1()
+    {
+        $response = $this->get(route('pattern'));
+
+        // dd($response->baseResponse->getOriginalContent());
+
+        $response->assertOk();
+    }
+
+    public function test_the_application_returns_a_successful_response_if_code_200_2()
+    {
+        $response = $this->get(route('pattern2'));
+
+        // dd($response->baseResponse->getOriginalContent());
+
+        $response->assertOk();
+    }
+
+    public function test_the_application_returns_a_successful_response_if_code_200_3()
+    {
+        $response = $this->get(route('pattern3'));
+
+        // dd();
+
+        $response->assertOk();
     }
 }
