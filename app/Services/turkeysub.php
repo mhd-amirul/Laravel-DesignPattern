3fc324793599baacf8dc54b16f2323647d95b66f
mhd-amirul <amirulm236@gmail.com>
2023-02-07 00:07:12 +0800

    feat: make child class for makeSub class

---
+++ app/Services/turkeysub.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+
+class TurkeySub extends Sub
+{
+
+    public function addprimarytoppings()
+    {
+        var_dump("step 3. add the turkey.");
+
+        return $this;
+    }
+}
