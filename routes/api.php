5f1d52aa5de3e5b9e4f731962f0fbb02c6b3de10
mhd-amirul <amirulm236@gmail.com>
2023-02-06 23:56:32 +0800

    feat: api route for design pattern

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppBasicPatternController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("basic-pattern", [AppBasicPatternController::class, "basic_pattern"])->name("pattern");
+Route::get("basic-pattern-2", [AppBasicPatternController::class, "implementation_adapter"])->name("pattern2");
+Route::get("basic-pattern-3", [AppBasicPatternController::class, "template_pattern"])->name("pattern3");
